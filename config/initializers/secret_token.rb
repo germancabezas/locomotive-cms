bb819eab9ba571d6f70f6d9ef0d6fe889f6644b9
GERMAN CABEZAS <GERMAN.CABEZAS@LIVE.COM>
2015-11-17 23:19:11 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MmDocs::Application.config.secret_token = 'c85b031cc776358b96fee393c2d79a6326c8e96009288a497daf1537a73d5b69cb143ac6618dabf10b82dfa52c28abe15b41d31ae9d3e1b03c66ffd15ce35dbe'
